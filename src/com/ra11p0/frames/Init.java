01fa7823687c14d854b4533f494334e7c8c13439
ra11p0 <83410596+ra11p0@users.noreply.github.com>
2021-11-12 18:00:42 +0100

    checking files module finished

--- src/com/ra11p0/frames/Init.java
+++ src/com/ra11p0/frames/Init.java
@@ -4,7 +4,6 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 import com.ra11p0.frames.Overview.Frames.Overview;
 import com.ra11p0.frames.ReceiptsManager.ReceiptsManager;
 import com.sun.xml.internal.ws.api.ResourceLoader;
-import jdk.nashorn.internal.scripts.JO;
 import org.apache.commons.io.FileUtils;
 
 import javax.swing.*;
@@ -13,9 +12,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
 import java.nio.file.Files;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Init {
     private final String DEFAULT_RECEIPT_PATH = "res\\receipts.json";
@@ -25,16 +22,13 @@ public class Init {
     private static Overview overview;
     public static Boolean reloading = false;
     public Init(String title) throws Exception{
+        //preparing last stuff before launching
         loadSettings();
-        if(settingsProp==null) {
-            settingsProp = new Properties();
-            settingsProp.loadFromXML(ResourceLoader.class.getResourceAsStream("/defaultSettings.xml"));
-        }
         //Check if settings file is right, if not, loading default settings.
         if(settingsProp.getProperty("language") == null ||
                 settingsProp.getProperty("currency") == null
         ){
-            JOptionPane.showMessageDialog(null, "Settings file is corrupted! Loading default settings.", "Error!", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Settings file is corrupted or empty! Loading default settings.", "Error!", JOptionPane.ERROR_MESSAGE);
             settingsProp = new Properties();
             settingsProp.loadFromXML(ResourceLoader.class.getResourceAsStream("/defaultSettings.xml"));
         }
@@ -42,6 +36,7 @@ public class Init {
         Locale locale = new Locale(settingsProp.getProperty("language"));
         localeBundle = ResourceBundle.getBundle("lang", locale);
         UIManager.setLookAndFeel( new FlatDarculaLaf());
+        //*****
         JFrame preparingFiles = new JFrame();
         preparingFiles.setResizable(false);
         preparingFiles.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -52,26 +47,7 @@ public class Init {
         new Thread(() -> {
             //loading receipts file from disk
             File temp = new File("res/.temp");
-            if(temp.exists()) {
-                try {
-                    FileUtils.cleanDirectory(temp);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                temp.delete();
-            }
-            //TODO - check all files using one method
             File source = new File("res/receipts.json");
-            File res = new File("res");
-            if(!res.exists()) res.mkdir();
-            if(!source.exists()) {
-                try {
-                    byte[] strToBytes = "".getBytes();
-                    Files.write(source.toPath(), strToBytes);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
             try {
                 FileUtils.copyFile(source, new File(temp.getPath() + "/receipts.json"));
             } catch (IOException e) {
@@ -91,7 +67,7 @@ public class Init {
                 @Override
                 public void windowClosed(WindowEvent e) {
                     try {
-                        settingsProp.storeToXML(new FileOutputStream(new File("settings.xml")), "");
+                        saveSettings();
                     } catch (IOException ex) {
                         ex.printStackTrace();
                     }
@@ -103,11 +79,14 @@ public class Init {
             e.printStackTrace();
         }
     }
-    public static void loadSettings() throws IOException {
-        if(!new File("settings.xml").exists()) return;
+    private static void loadSettings() throws IOException {
         settingsProp = new Properties();
         InputStream is = new FileInputStream("settings.xml");
         settingsProp.loadFromXML(is);
         is.close();
     }
+    private static void saveSettings() throws IOException {
+        settingsProp.storeToXML(new FileOutputStream(new File("settings.xml")), "");
+    }
+
 }
